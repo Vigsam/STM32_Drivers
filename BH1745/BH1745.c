3cdce08ccf37178592a0a06e7ac2e2601f4a208e
Vigneshwaran S <30697802+Vigsam@users.noreply.github.com>
2021-11-03 15:29:10 +0530

    Update BH1745.c

--- BH1745/BH1745.c
+++ BH1745/BH1745.c
@@ -1,196 +1,194 @@
 /*
  * BH1745.c
  *
- *  Created on: Feb 16, 2021
- *      Author: VIGNESH
+ *  Created on: Nov 3, 2021
+ *      Author: VIGNESHWARAN
  */
 
-#include "main.h"
+
 #include "BH1745.h"
-#include "stdio.h"
-
-//Function Prototype 1. Reset BH1745
 
 
-int Reset_BH1745(void)
+uint8_t BH1745_Reset(){
 
-{
-	int state;
+uint8_t status;
 
-	uint8_t tx_buffer = 0x8B;
+uint8_t tx_buffer = 0x8B;
 
-	uint8_t rx_buffer[2] = {0,0};
+uint8_t rx_buffer;
 
-	state = HAL_I2C_Mem_Write(&hi2c1, BH1745_ADDR, SYS_CNTRL_ADDR, I2C_MEMADD_SIZE_8BIT, &tx_buffer, 1, HAL_MAX_DELAY);
+status = HAL_I2C_Mem_Write(&hi2c1, BH1745_SLAVE_ADDR, BH1745_CTRL_REG_ADDR, I2C_MEMADD_SIZE_8BIT, &tx_buffer, 1, HAL_MAX_DELAY);
 
-	if(state != HAL_OK)
-	{
-		return 0;
-	}
-
-	HAL_I2C_Mem_Read(&hi2c1, BH1745_ADDR, SYS_CNTRL_ADDR, I2C_MEMADD_SIZE_8BIT, rx_buffer, 1, 100);
-
-	if(rx_buffer[0] == 0x0B)
-	{
-		return OK;
-	}
-
-
-	return 0;
-}
-
-//Function Prototype 2. Initializing BH1745
-
-int Initialize_BH1745(void){
-
-
-	int state;
-
-	uint8_t rx_buff[2] = {0,0};
-
-	state = HAL_I2C_Mem_Read(&hi2c1, BH1745_ADDR, MANUFAC_ID_ADDR, I2C_MEMADD_SIZE_8BIT, rx_buff, 1, HAL_MAX_DELAY);
-
-	if(state != HAL_OK)
-	{
-		return 0;
-	}
-
-	if(rx_buff[0] == 0xE0)
-	{
-		return OK;
-	}
-
-
-	return 0;
-}
-
-
-//Function Prototype 3. Setting measurement Time to 640 milliseconds
-
-int set_measurement_time(void)
-{
-
-	int state;
-
-	uint8_t tx_buffer = 0x02;
-
-	uint8_t rx_buffer[2] = {0,0};
-
-	state = HAL_I2C_Mem_Write(&hi2c1, BH1745_ADDR, MODE_CONTROL1_ADDR, I2C_MEMADD_SIZE_8BIT, &tx_buffer, 1, HAL_MAX_DELAY);
-
-	if(state != HAL_OK)
-	{
-		return 0;
-	}
-
-	HAL_I2C_Mem_Read(&hi2c1, BH1745_ADDR, MODE_CONTROL1_ADDR, I2C_MEMADD_SIZE_8BIT, rx_buffer, 1, 100);
-
-	if(rx_buffer[0] == 0x02)
-	{
-		return OK;
-	}
+if(status != HAL_OK){
 
 	return 0;
 
 }
 
-//Function Prototype 4: Activate BH1745 for Measurement
+status = HAL_I2C_Mem_Read(&hi2c1, BH1745_SLAVE_ADDR, BH1745_CTRL_REG_ADDR, I2C_MEMADD_SIZE_8BIT, &rx_buffer, 1, HAL_MAX_DELAY);
 
+if(rx_buffer == 0x0B){
 
-int activate_measument(void)
+	return HAL_OK;
+}
+
+return 0;
+
+}
+
+uint8_t BH1745_Initialize(){
+
+uint8_t rx_buffer;
+
+HAL_I2C_Mem_Read(&hi2c1, BH1745_SLAVE_ADDR, BH1745_ID_ADDR, I2C_MEMADD_SIZE_8BIT, &rx_buffer, 1, HAL_MAX_DELAY);
+
+if(rx_buffer == 0xE0)
 {
-	int state;
+	return HAL_OK;
+}
 
-	uint8_t tx_buffer = 0x10;
+return 0;
 
+}
+
+uint8_t BH1745_Set_Measurement_time(uint8_t measure_time){
+
+uint8_t status = HAL_I2C_Mem_Write(&hi2c1, BH1745_SLAVE_ADDR, BH1745_MODE_CTRL1, I2C_MEMADD_SIZE_8BIT, &measure_time, 1 , HAL_MAX_DELAY);
+
+if(status != HAL_OK)
+{
+	return 0;
+}
+
+HAL_I2C_Mem_Read(&hi2c1, BH1745_SLAVE_ADDR, BH1745_MODE_CTRL1, I2C_MEMADD_SIZE_8BIT, &measure_time, 1 , HAL_MAX_DELAY);
+
+if(measure_time == BH1745_MEASURE_TIME_160)
+{
+	return HAL_OK;
+}
+
+else if(measure_time == BH1745_MEASURE_TIME_320)
+{
+	return HAL_OK;
+}
+
+else if(measure_time == BH1745_MEASURE_TIME_640)
+{
+	return HAL_OK;
+}
+
+else if(measure_time == BH1745_MEASURE_TIME_1280)
+{
+	return HAL_OK;
+}
+
+else if(measure_time == BH1745_MEASURE_TIME_2560)
+{
+	return HAL_OK;
+}
+
+else if(measure_time == BH1745_MEASURE_TIME_5120)
+{
+	return HAL_OK;
+}
+
+return 0;
+
+}
+
+uint8_t BH1745_Activate_Measurement(uint8_t adc_gain){
+
+uint8_t tx_buffer = 0x02;
+
+uint8_t status;
+
+status = HAL_I2C_Mem_Write(&hi2c1, BH1745_SLAVE_ADDR, BH1745_MODE_CTRL2, I2C_MEMADD_SIZE_8BIT, &adc_gain, 1 , HAL_MAX_DELAY);
+
+if(status != HAL_OK)
+{
+	return 0;
+}
+
+status = HAL_I2C_Mem_Write(&hi2c1, BH1745_SLAVE_ADDR, BH1745_MODE_CTRL3, I2C_MEMADD_SIZE_8BIT, &tx_buffer, 1 , HAL_MAX_DELAY);
+
+if(status != HAL_OK)
+{
+	return 0;
+}
+
+HAL_I2C_Mem_Read(&hi2c1, BH1745_SLAVE_ADDR, BH1745_MODE_CTRL2, I2C_MEMADD_SIZE_8BIT, &adc_gain, 1 , HAL_MAX_DELAY);
+
+if(adc_gain == BH1745_ADC_GAIN_1X)
+{
+	return HAL_OK;
+}
+
+if(adc_gain == BH1745_ADC_GAIN_2X)
+{
+	return HAL_OK;
+}
+
+if(adc_gain == BH1745_ADC_GAIN_16X)
+{
+	return HAL_OK;
+}
+
+return 0;
+
+}
+
+uint16_t BH1745_GetData_Red(uint32_t delay)
+{
 	uint8_t rx_buffer[2] = {0,0};
 
-	state = HAL_I2C_Mem_Write(&hi2c1, BH1745_ADDR, MODE_CONTROL2_ADDR, I2C_MEMADD_SIZE_8BIT, &tx_buffer, 1, HAL_MAX_DELAY);
+	uint16_t reg_value;
 
-	if(state != HAL_OK)
+	uint8_t status = HAL_I2C_Mem_Read(&hi2c1, BH1745_SLAVE_ADDR, BH1745_RED_LSB, I2C_MEMADD_SIZE_8BIT, rx_buffer, 2 , delay);
+
+	if(status != HAL_OK)
 	{
 		return 0;
 	}
 
-	HAL_I2C_Mem_Read(&hi2c1, BH1745_ADDR, MODE_CONTROL2_ADDR, I2C_MEMADD_SIZE_8BIT, rx_buffer, 1, 100);
+	reg_value = ((rx_buffer[1] << 8) | (rx_buffer[0]));
 
-	if((rx_buffer[0] == 0x10) || (rx_buffer[0] == 0x90))
+	return reg_value;
+}
+
+uint16_t BH1745_GetData_Green(uint32_t delay)
+{
+	uint8_t rx_buffer[2] = {0,0};
+
+	uint16_t reg_value;
+
+	uint8_t status = HAL_I2C_Mem_Read(&hi2c1, BH1745_SLAVE_ADDR, BH1745_GREEN_LSB, I2C_MEMADD_SIZE_8BIT, rx_buffer, 2 , delay);
+
+	if(status != HAL_OK)
 	{
-		return OK;
+		return 0;
 	}
+
+	reg_value = ((rx_buffer[1] << 8) | (rx_buffer[0]));
+
+	return reg_value;
 
 	return 0;
-
 }
 
-
-//Function Prototype 5: Read RED
-
-
-uint16_t Read_RedData(void)
+uint16_t BH1745_GetData_Blue(uint32_t delay)
 {
-	int state;
-
 	uint8_t rx_buffer[2] = {0,0};
 
-	uint16_t reg;
+	uint16_t reg_value;
 
-	state = HAL_I2C_Mem_Read(&hi2c1, BH1745_ADDR, RED_DATA_LSB_ADDR, I2C_MEMADD_SIZE_8BIT, rx_buffer, 1, 640);
+	uint8_t status = HAL_I2C_Mem_Read(&hi2c1, BH1745_SLAVE_ADDR, BH1745_BLUE_LSB, I2C_MEMADD_SIZE_8BIT, rx_buffer, 2 , delay);
 
-	if(state != HAL_OK)
+	if(status != HAL_OK)
 	{
 		return 0;
 	}
 
-	reg = ((rx_buffer[1] << 8) | (rx_buffer[0]));
+	reg_value = ((rx_buffer[1] << 8) | (rx_buffer[0]));
 
-	return reg;
-
-}
-
-//Function Prototype 6: Read Green
-
-uint16_t Read_GreenData(void)
-{
-	int state;
-
-	uint8_t rx_buffer[2] = {0,0};
-
-	uint16_t reg;
-
-	state = HAL_I2C_Mem_Read(&hi2c1, BH1745_ADDR, GREEN_DATA_LSB_ADDR, I2C_MEMADD_SIZE_8BIT, rx_buffer, 1, 640);
-
-	if(state != HAL_OK)
-	{
-		return 0;
-	}
-
-	reg = ((rx_buffer[1] << 8) | (rx_buffer[0]));
-
-	return reg;
-
-}
-
-//Function Prototype 7: Read Blue
-
-
-uint16_t Read_BlueData(void)
-{
-	int state;
-
-	uint8_t rx_buffer[2] = {0,0};
-
-	uint16_t reg;
-
-	state = HAL_I2C_Mem_Read(&hi2c1, BH1745_ADDR, BLUE_DATA_LSB_ADDR, I2C_MEMADD_SIZE_8BIT, rx_buffer, 1, 640);
-
-	if(state != HAL_OK)
-	{
-		return 0;
-	}
-
-	reg = ((rx_buffer[1] << 8) | (rx_buffer[0]));
-
-	return reg;
-
+	return reg_value;
 }
